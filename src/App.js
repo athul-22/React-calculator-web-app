c90b63cd382bce9b54ed4d0a86ec88140d51584b
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-01-24 07:37:02 +0530

    V4

--- src/App.js
+++ src/App.js
@@ -3,21 +3,35 @@ import './App.css';
 import './styles.css';
 import DigitBtn from "./DigitBtn";
 import  {Component} from "react";
+import { useReducer } from 'react';
+
+const ACTIONS = {
+  ADD_DIGIT : 'add-digit',
+  CHOOSE_OPERATION : 'choose-operation',
+  CLEAR : 'clear',
+  DELETE_DIGIT : 'delete-digit',
+  EVALUATE : 'evaluate'
+}
 
 
-
+function reducer(state,{type , payload}){
+  switch(type) {
+    
+  }
+}
 
 
 function App() {
 
- 
+const [{currentOperand, previousOperand, operation},dispatch] = useReducer(reducer)
+
 
   return (
     <div className="cal-grid">
       <div className="main">
       <div className='output'>
-        <div className='previous-operand'>1233</div>
-        <div className='current-operand'>1222</div>
+        <div className='previous-operand'>{previousOperand} {operation}</div>
+        <div className='current-operand'>{currentOperand}</div>
       </div>
       <button className='span-2'>AC</button>
       <button className='del'>DEL</button>
