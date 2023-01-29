390c43816b76a4aedf34b3cc6059dadcd663d23c
Athul Nambiar <dev.athulnambiar@gmail.com>
2023-01-29 09:11:18 +0530

    UI

--- src/DarkMode/ToggleBtn.js
+++ src/DarkMode/ToggleBtn.js
@@ -4,6 +4,8 @@ import './darkmodestyles.css';
 import { WiDaySunny } from "react-icons/wi";
 import { BsMoon } from "react-icons/bs";
 
+function ToggleBtn() {
+
 const DarkMode = ()=>{
   let clickedClass = "clicked";
   const body = document.body;
@@ -11,22 +13,25 @@ const DarkMode = ()=>{
   const lightTheme = "light";
   const darkTheme = "darkTheme";
   let theme;
-}
 
-if(localStorage){
-  theme = localStorage.getItem("theme")
-}
-
-if(theme === lightTheme || theme === darkTheme){
-  body.classList.add(theme);
-}
-else{
-  body.classList.add(theme);
-}
-
+  if(localStorage){
+    theme = localStorage.getItem("theme")
+  }
+  
+  if(theme === lightTheme || theme === darkTheme){
+    body.classList.add(theme);
+  }
+  else{
+    body.classList.add(theme);
+  }
 
 
-function ToggleBtn() {
+  const switchTheme = (e) => {
+    if(theme === darkTheme) {
+      body.classList.replace(darkTheme, lightTheme);
+    }
+  };
+
   return (
     <div className='toggle-btns'>
         <button 
@@ -38,6 +43,7 @@ function ToggleBtn() {
         <button className='night'><BsMoon/></button></center>
     </div>
   )
-}
+};
+};
 
 export default ToggleBtn
